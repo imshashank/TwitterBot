8c9d35b8f8ebcd453b315104213490b7c7c1ae9d
shashank agarwal <shashank@shankee.com>
2014-03-05 17:56:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from auto import auto_fav
 #this is a sample program to use the auto.py script
 
 auto_fav("ubuntu", count=1)
-
 auto_fav("teamfollow", count=1)
-
 auto_fav("teamfollowback", count=0)
 
+#auto follow 10 users who come when we search for keyword "followback"
 auto_follow("followback", count=10)
 auto_follow("teamfollow", count=5)
 
