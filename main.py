beaa6d5a57f53acc778976e2d81650baaca5c886
shashank agarwal <shashank@shankee.com>
2014-03-07 12:14:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from auto import auto_follow
 from auto import auto_fav
 
 #this is a sample program to use the auto.py script
-
 auto_fav("ubuntu", count=1)
 auto_fav("teamfollow", count=1)
 auto_fav("teamfollowback", count=0)
